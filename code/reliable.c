6f85f426089eb938a4b6d80be5d31ada2e98d02d
anna-peter <annamei.peter@gmx.ch>
2021-04-12 13:16:55 +0200

    debugs

--- code/reliable.c
+++ code/reliable.c
@@ -147,7 +147,8 @@ rel_destroy (rel_t *r)
 void
 rel_recvpkt (rel_t *r, packet_t *pkt, size_t n)
 {
-    fprintf(stderr,"rel_recvpkt was called with packet no "++ ntohl(pkt->ackno));
+    fprintf(stderr,"rel_recvpkt was called with packet ackno ");
+    fprintf(ntohl(pkt->ackno));
     if (is_corrupted(pkt)) {
         fprintf(stderr, "packet was corrupted\n");
         packet_t* ack = create_ack(r->rcv_nxt);
